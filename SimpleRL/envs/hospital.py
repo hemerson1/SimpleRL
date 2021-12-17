066a20ff896ea9542db7ae021a7f576927858284
hemerson1 <emersonharry8@gmail.com>
2021-12-17 19:56:42 +0000

    Created staff generator

--- SimpleRL/envs/hospital.py
+++ SimpleRL/envs/hospital.py
@@ -25,14 +25,23 @@ class hospital_env:
     def __init__(self):
         
         # TODO: how is the state of the enviornment going to be represented?
-        # TODO: what are the actions going to be? 
+        # TODO: what are the actions going to be? (assigning staff to patients)
         
         # define the illness parameters (for mild, moderate & severe)
-        self.illness_likelihood = [0.5, 0.3, 0.2]
-        death_probs = {"mild": 0.05, "moderate": 0.15, "severe": 0.25}
-        recovery_time = {"mild": 2, "moderate": 4, "severe": 6}
+        self.illness_likelihood = [0.5, 0.3, 0.2] # liklihood of a patient class
+        self.death_probs = {"mild": 0.05, "moderate": 0.15, "severe": 0.25} # mean likelihood of death per class
+        self.recovery_time = {"mild": 2, "moderate": 4, "severe": 6} # mean recovery time for each class
         
-        # define the hospital parameters        
+        # define the staff parameters (for doctors and nurses)
+        self.staff_number = 10 # number of staff at the hospital
+        self.staff_split = [0.5, 0.5] # ratio of doctors to nurses
+        self.staff_efficiency = {"doctor": 2, "nurse": 1} # healing bonus for non-specific tasks 
+        self.staff_roles = {"doctor": ["diagnosis", "treatment"], "nurse": ["treatment"]}
+        
+        # define the hospital parameters       
+        self.available_diagnosis_rooms = 5
+        self.available_treatment_rooms = 5
+        
         self.hospital_staff = []
         self.hospital_patients = []        
         
--- SimpleRL/utils/hospital_utils.py
+++ SimpleRL/utils/hospital_utils.py
@@ -8,7 +8,7 @@ Created on Thu Dec 16 21:30:44 2021
 
 import numpy as np
 
-def generate_patient(illness_likelihood, death_probs, recovery_time):
+def generate_patient(illness_likelihood, death_probs, recovery_time, id_number):
     """
     creates a random patient for the hospital and assigns them an illness and 
     a probability of death
@@ -38,16 +38,88 @@ def generate_patient(illness_likelihood, death_probs, recovery_time):
     # update the patient records
     patient_info['disease_class'] = patient_class
     patient_info['death_prob'] = patient_death_prob  
-    patient_info['recovery_time'] = patient_recovery_time       
-    
+    patient_info['recovery_time'] = patient_recovery_time     
+        
     # has the patient been diagnosed
     patient_info['diagnosed'] = False 
     
     return patient_info
     
+def generate_staff(staff_split, staff_efficiency, staff_roles, staff_number=10):
+    """
+    creates a random team of staff members for the hospital each with different roles
+    
+    Parameters:
+    ----------
+        
+    Return:
+    ------
+            
+    """
+    
+    staff_roster = []
+    staff_classifcation = ["doctor", "nurse"]
+        
+    for stf in range(staff_number):
+        
+        # initialise the staff record
+        staff_member_info = dict()
+        
+        # get staff title 
+        staff_member_title = np.random.choice(staff_classifcation, size=1, p=staff_split)[0]    
+        
+        # get staff specific parameters
+        staff_member_efficiency = staff_efficiency[staff_member_title]
+        staff_member_roles = staff_roles[staff_member_title]
+        
+        # update their info
+        staff_member_info['id'] = stf
+        staff_member_info["title"] = staff_member_title
+        staff_member_info["efficiency"] = staff_member_efficiency
+        staff_member_info["roles"] = staff_member_roles 
+        
+        # add the treatment related info
+        staff_member_info["current_role"] = None
+        staff_member_info["patient_id"] = None
+        
+        # add them to the roster
+        staff_roster.append(staff_member_info)
+       
+    return staff_roster
+
+
 if __name__ == "__main__": 
     
-    patient_info = generate_patient()
+    # define the illness parameters
+    id_number = 1
+    illness_likelihood = [0.5, 0.3, 0.2]
+    death_probs = {"mild": 0.05, "moderate": 0.15, "severe": 0.25}
+    recovery_time = {"mild": 2, "moderate": 4, "severe": 6}
     
+    # get patient info
+    patient_info = generate_patient(illness_likelihood=illness_likelihood, 
+                                    death_probs=death_probs, 
+                                    recovery_time=recovery_time, 
+                                    id_number = id_number
+                                    )
+    
+    print('\nTest Patient Profile:')
     print(patient_info)
+    
+    # define the staff parameters
+    staff_number = 10
+    staff_split = [0.5, 0.5]
+    staff_efficiency = {"doctor": 2, "nurse": 1}
+    staff_roles = {"doctor": ["diagnosis", "treatment"], "nurse": ["treatment"]}
+    
+    # get the staff information
+    staff_roster = generate_staff(staff_split=staff_split,
+                                  staff_efficiency=staff_efficiency,
+                                  staff_roles=staff_roles,
+                                  staff_number=staff_number
+                                  )
+    
+    print('\nTest Staff Profile:')
+    print(staff_roster[0])
+    
     
