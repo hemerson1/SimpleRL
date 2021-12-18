770a321240195d39c2a822310a24ffd39f84f300
hemerson1 <emersonharry8@gmail.com>
2021-12-18 15:03:04 +0000

    Implemented basic hospital algorithm

--- SimpleRL/envs/hospital.py
+++ SimpleRL/envs/hospital.py
@@ -21,30 +21,41 @@ Patients must be first diagnosed and then assigned to the relevant treatment.
 
 """
 
+import numpy as np
+
 from SimpleRL.utils.hospital_utils import generate_patient, generate_staff, generate_rooms
 
 class hospital_env:
-    def __init__(self):
+    def __init__(self, seed=None):
         
         # TODO: how is the state of the enviornment going to be represented?
         # TODO: what are the actions going to be? (assigning staff to patients)
+        # TODO: add seeding
+        # TODO: add rendering
         
         # define the environmental parameters
-        self.max_timestep = (24 * 60) / 10 # 24 hrs in 10 minute intervals
+        self.seed = seed
+        np.random.seed(self.seed)  
+        self.max_timestep = 30 * 6 # 6 months
         
         # define the illness parameters (for mild, moderate & severe)
+        self.disease_classification = ["mild", "moderate", "severe"]
         self.illness_likelihood = [0.5, 0.3, 0.2] # liklihood of a patient class
         self.death_probs = {"mild": 0.05, "moderate": 0.15, "severe": 0.25} # mean likelihood of death per class
         self.recovery_time = {"mild": 2, "moderate": 4, "severe": 6} # mean recovery time for each class
         
         # define the patient paramaters
-        self.patient_spawn_prob = 0.3 # probability of patient per timestep 
+        self.patient_spawn_prob = 1.0 # probability of patient per timestep 
         
         # define the staff parameters (for doctors and nurses)
         self.staff_number = 10 # number of staff at the hospital
         self.staff_split = [0.5, 0.5] # ratio of doctors to nurses
         self.staff_efficiency = {"doctor": 2, "nurse": 1} # healing bonus for non-specific tasks 
         self.staff_roles = {"doctor": ["diagnosis", "treatment"], "nurse": ["treatment"]}
+        
+        # define the reward
+        self.death_penalty = -100
+        self. waiting_penalty = -1
         
         # define the hospital parameters    
         self.number_rooms = 10
@@ -55,18 +66,34 @@ class hospital_env:
     def reset(self):
         
         # reset the staff roster
-        self.hospital_staff = generate_staff(
+        self.hospital_staff, self.doctor_ids = generate_staff(
             staff_split=self.staff_split, 
             staff_efficiency=self.staff_efficiency, 
             staff_roles=self.staff_roles
             )
         
+        print('Doctor ids: {}'.format(self.doctor_ids))
+        
         # reset the current patients in the hospital
         self.hospital_patients = []
+        self.current_patient_id = 0
+        self.day_counter = 0
         
-        self.room_arrangement = {}
+        # get the score tallys
+        self.total_deaths = 0
+        self.total_days_waited = 0
+        self.total_recoveries = 0
         
-    def step(self, action=None): 
+        # set up the room arrangement
+        self.room_arrangement = generate_rooms(number_rooms=self.number_rooms)
+        
+        state = np.zeros((self.number_rooms, 3))
+        
+        return state.flatten()
+        
+        
+    def step(self, action=None):
+        
         """
         state:
         ------
@@ -97,12 +124,213 @@ class hospital_env:
         1d array:
         number of doctors + number of nurses (give room number 0, 1, ...)        
         n = n (e.g. 10 actions)        
-        
-        
         """
-        pass
+        
+        # initialise the reward tallys
+        waiting_today = 0
+        deaths_today = 0    
+        recovered_today = 0
+        
+        # update the rooms with the staff choices -------------------
+        
+        # clear the current staff arrangements
+        for idx, _ in enumerate(self.room_arrangement):
+            self.room_arrangement[idx]['staff_ids'] = []            
+        
+        # cycle through the staff assignments
+        for staff_idx, room_num in enumerate(action):
+            
+            # add the ids of the staff to the relevant rooms
+            self.room_arrangement[room_num]["staff_ids"].append(staff_idx)                  
+            
+        # add the changes as a result of staff presence -------------
+        
+        for room_idx, room in enumerate(self.room_arrangement):
+            
+            # if there is a patient in the room
+            if room["patient_id"] != None:
+                
+                current_patient = None
+                current_patient_idx = None
+                                
+                # find the patient's medical record
+                for idx, patient in enumerate(self.hospital_patients):                  
+                    
+                    if patient["id"] == room["patient_id"]:
+                        current_patient = patient
+                        current_patient_idx = idx
+                        break
+                
+                # cycle through staff and allow doctors actions first
+                for stf in room['staff_ids']:
+                    
+                    # if this staff member is a doctor
+                    if stf in self.doctor_ids:
+                        
+                        # diagnose the patient
+                        if not current_patient['diagnosed']:
+                            current_patient['diagnosed'] = True
+                            continue
+                    
+                        # allow treatment if patient has been diagnosed                    
+                        if current_patient['diagnosed']:                        
+                            # treat the patient
+                            current_patient['recovery_time'] -= self.staff_efficiency['doctor']
+                
+                # now allow nurses' actions
+                for stf in room['staff_ids']:
+                    
+                    # if this staff member is a doctor
+                    if stf not in self.doctor_ids and current_patient['diagnosed']:
+                 
+                        # treat the patient
+                        current_patient['recovery_time'] -= self.staff_efficiency['nurse']
+                
+                # remove the patient if they have recovered
+                recovered = False                
+                if current_patient['recovery_time'] <= 0:
+                    
+                    print('Patient {} has recovered'.format(current_patient['id']))
+                    
+                    # remove from hospital patients
+                    self.hospital_patients.pop(current_patient_idx)
+                    
+                    # update the room
+                    self.room_arrangement[room_idx]["patient_id"] = None       
+                    
+                    recovered = True
+                    
+                    # update recovered
+                    recovered_today += 1
+                
+                # update the illness of the patient     
+                self.hospital_patients[current_patient_idx]['waiting_time'] += 1
+                waiting_today += 1
+                
+                # update the days deaths
+                if np.random.uniform(0, 1, 1) < current_patient['death_prob'] and not recovered:
+                    
+                    print('Patient {} has died'.format(current_patient['id']))
+                    
+                    # update deaths
+                    deaths_today += 1
+                    
+                    # remove from hospital patients
+                    self.hospital_patients.pop(current_patient_idx)
+                    
+                    # update the room
+                    self.room_arrangement[room_idx]["patient_id"] = None 
+                    
+                            
+        # generate a patient ----------------------------------------
+              
+        if np.random.uniform(0, 1, 1) < self.patient_spawn_prob and len(self.hospital_patients) < self.number_rooms:
+            
+            # create new patient
+            new_patient = generate_patient(
+                disease_classification=self.disease_classification,
+                illness_likelihood=self.illness_likelihood, 
+                death_probs=self.death_probs, 
+                recovery_time=self.recovery_time, 
+                id_number = self.current_patient_id
+                )
+        
+            # add patients to the rooms
+            for room in self.room_arrangement:
+                
+                # add patient ID to room and update current_id
+                if room['patient_id'] == None:
+                    
+                    # add patient id to room
+                    room['patient_id'] = self.current_patient_id
+                    
+                    # add room number to patient
+                    new_patient['room_number'] = room['room_number']
+                    
+                    # add patient to the hospital list
+                    self.hospital_patients.append(new_patient)
+                    
+                    # update patient_id
+                    self.current_patient_id += 1
+                    
+                    break
+                
+        # visualise the next state --------------------------------
+        
+        state = np.zeros((self.number_rooms, 3))
+        
+        for idx, room in enumerate(self.room_arrangement):
+            
+            # if there is a patient in the room
+            if room["patient_id"]:
+                
+                current_patient = None
+                
+                # find the patient's medical record
+                for idx, patient in enumerate(self.hospital_patients):                    
+                    if patient["id"] == room["patient_id"]:
+                        current_patient = patient
+                
+                # get the disease class, days waiting and treatment/diagnosis stage
+                state[idx, 0] = self.disease_classification.index(current_patient["disease_class"]) + 1
+                state[idx, 1] = current_patient["waiting_time"]
+                state[idx, 2] = int(current_patient["diagnosed"]) + 1
+        
+        # calculate the reward -----------------------------------
+        
+        self.total_days_waited += waiting_today
+        self.total_deaths += deaths_today
+        self.total_recoveries += recovered_today
+        
+        reward = self.death_penalty * deaths_today + self.waiting_penalty * waiting_today
+        
+        # get done -----------------------------------------------
+        
+        done = False
+        self.day_counter += 1
+        if self.day_counter == self.max_timestep:
+            done = True
+        
+        info = {}
+            
+        return state, reward, done, info        
+            
         
 if __name__ == "__main__": 
-
-    env = hospital_env()
+    
+    test_days = 10
+    staff_number = 10
+    
+    # initialise the environment
+    env = hospital_env(seed=0)
+    
+    for days in range(test_days):
+    
+        # test action with each element representing a person 
+        # and the idx representing their room assignment        
+        
+        action = np.random.randint(10, size=10)  
+        
+        print('Chosen_action: {}'.format(action))
+        
+        print('\nDay {} ----------'.format(days))
+        
+        # take a step
+        env.step(action=action)
+        
+        print('\nRoom Arrangement:')
+        print(env.room_arrangement)
+        
+        print('\nCurrent Patients:')
+        print(env.hospital_patients)
+        print('---------------------------')
+    
+    print('\nSUMMARY ----------------------------------')
+    print('Total patients seen: {}'.format(env.current_patient_id + 1))
+    print('Total deaths: {}'.format(env.total_deaths))
+    print('Total recoveries: {}'.format(env.total_recoveries))
+    print('Total days waiting: {}'.format(env.total_days_waited))
+    print('-------------------------------------------')
+        
+    
         
--- SimpleRL/utils/hospital_utils.py
+++ SimpleRL/utils/hospital_utils.py
@@ -8,7 +8,7 @@ Created on Thu Dec 16 21:30:44 2021
 
 import numpy as np
 
-def generate_patient(illness_likelihood, death_probs, recovery_time, id_number):
+def generate_patient(disease_classification, illness_likelihood, death_probs, recovery_time, id_number):
     """
     creates a random patient for the hospital and assigns them an illness and 
     a probability of death
@@ -25,7 +25,6 @@ def generate_patient(illness_likelihood, death_probs, recovery_time, id_number):
     patient_info = dict()
     
     # select a disease classification
-    disease_classification = ["mild", "moderate", "severe"]
     patient_class = np.random.choice(disease_classification, size=1, p=illness_likelihood)[0]
     
     # get their probability of death per timestep
@@ -62,6 +61,7 @@ def generate_staff(staff_split, staff_efficiency, staff_roles, staff_number=10):
     """
     
     staff_roster = []
+    doctor_ids = []
     staff_classifcation = ["doctor", "nurse"]
         
     for stf in range(staff_number):
@@ -86,10 +86,13 @@ def generate_staff(staff_split, staff_efficiency, staff_roles, staff_number=10):
         staff_member_info["current_role"] = None
         staff_member_info["room_number"] = None
         
+        if staff_member_title == "doctor":
+            doctor_ids.append(stf)
+        
         # add them to the roster
         staff_roster.append(staff_member_info)
        
-    return staff_roster
+    return staff_roster, doctor_ids
 
 
 def generate_rooms(number_rooms):
@@ -98,7 +101,7 @@ def generate_rooms(number_rooms):
     room_arrangement = []
     
     # create the diagnosis rooms
-    for room_num in range(1, number_rooms + 1):
+    for room_num in range(number_rooms):
         
         room = dict()
         room["room_number"] = room_num
@@ -113,17 +116,20 @@ def generate_rooms(number_rooms):
 if __name__ == "__main__": 
     
     # define the illness parameters
-    id_number = 1
+    id_number = 1    
+    disease_classification = ["mild", "moderate", "severe"]
     illness_likelihood = [0.5, 0.3, 0.2]
     death_probs = {"mild": 0.05, "moderate": 0.15, "severe": 0.25}
     recovery_time = {"mild": 2, "moderate": 4, "severe": 6}
     
     # get patient info
-    patient_info = generate_patient(illness_likelihood=illness_likelihood, 
-                                    death_probs=death_probs, 
-                                    recovery_time=recovery_time, 
-                                    id_number = id_number
-                                    )
+    patient_info = generate_patient(
+        disease_classification=disease_classification,
+        illness_likelihood=illness_likelihood, 
+        death_probs=death_probs, 
+        recovery_time=recovery_time, 
+        id_number = id_number
+        )
     
     print('\nTest Patient Profile:')
     print(patient_info)
@@ -135,14 +141,15 @@ if __name__ == "__main__":
     staff_roles = {"doctor": ["diagnosis", "treatment"], "nurse": ["treatment"]}
     
     # get the staff information
-    staff_roster = generate_staff(staff_split=staff_split,
-                                  staff_efficiency=staff_efficiency,
-                                  staff_roles=staff_roles,
-                                  staff_number=staff_number
-                                  )
+    staff_roster, doctor_ids = generate_staff(staff_split=staff_split, 
+                                               staff_efficiency=staff_efficiency,
+                                               staff_roles=staff_roles,
+                                               staff_number=staff_number
+                                               )
     
     print('\nTest Staff Profile:')
     print(staff_roster[0])
+    print(doctor_ids)
     
     # define the building parameters 
     number_rooms = 10
