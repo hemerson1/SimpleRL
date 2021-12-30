24251fe319d830a5baae369bb31d01a3bf4c427b
hemerson1 <emersonharry8@gmail.com>
2021-12-30 18:40:20 +0000

    Removed dependency of borrowed package on scipy

--- SimpleRL/utils/race_car_utils.py
+++ SimpleRL/utils/race_car_utils.py
@@ -18,16 +18,15 @@ import matplotlib.pyplot as plt
 # Used to compute the convex hull
 # https://startupnextdoor.com/computing-convex-hull-in-python/
 
+# Used for BSpline interpolation and evaluation:
+# https://github.com/XuejiaoYuan/BSpline
+
 def generate_track():
     pass
 
 import pygame, sys
 import random 
 
-# TODO: remove scipy import
-from scipy import interpolate
-
-  
 # Screen dimensions
 WIDTH = 800 
 HEIGHT = 600
@@ -73,7 +72,7 @@ SPLINE_POINTS = 1000
 # initial polygon
 MARGIN = 50
 # minimum distance between points that form the track skeleton
-MIN_DISTANCE = 20
+MIN_DISTANCE = 100 # 20
 # Maximum midpoint displacement for points placed after obtaining the initial polygon
 MAX_DISPLACEMENT = 80
 # Track difficulty
@@ -178,7 +177,7 @@ def convex_hull(points):
         
     return np.array(hull_points[:-1])
 
-def shape_track(track_points, difficulty=DIFFICULTY, max_displacement=MAX_DISPLACEMENT, margin=MARGIN):
+def shape_track(track_points, difficulty=DIFFICULTY, max_displacement=MAX_DISPLACEMENT, margin=MARGIN, track_width=TRACK_WIDTH):
     
     # create a list of zero pairs twice as long as hull points
     track_set = [[0,0] for i in range(len(track_points) * 2)] 
@@ -198,24 +197,33 @@ def shape_track(track_points, difficulty=DIFFICULTY, max_displacement=MAX_DISPLA
         # set second index to current track point + mean of current and next track point + magnitude of dispalcement
         track_set[i * 2 + 1][0] = int((track_points[i][0] + track_points[(i + 1) % len(track_points)][0]) / 2 + disp[0])
         track_set[i * 2 + 1][1] = int((track_points[i][1] + track_points[(i + 1) % len(track_points)][1]) / 2 + disp[1])
-        
-        
+       
+    
+    # ensure angles are suitable and points are sufficiently far apart
     for i in range(3):
         track_set = fix_angles(track_set)
         track_set = push_points_apart(track_set)
         
-    # push any point outside screen limits back again
+    # ensure all the points are within the screen limits
     final_set = []
     for point in track_set:
-        if point[0] < margin:
-            point[0] = margin
-        elif point[0] > (WIDTH - margin):
-            point[0] = WIDTH - margin
-        if point[1] < margin:
-            point[1] = margin
-        elif point[1] > HEIGHT - margin:
-            point[1] = HEIGHT - margin
+        
+        # if outside x dimension including track width
+        if point[0] - track_width < margin:
+            point[0] = margin + track_width            
+        elif point[0] + track_width > (WIDTH - margin):
+            point[0] = (WIDTH - margin) - track_width
+        
+        # if outside y dimension including track width        
+        if point[1] - track_width < margin:
+            point[1] = margin + track_width
+        elif point[1] + track_width > HEIGHT - margin:
+            point[1] = (HEIGHT - margin) - track_width
+            
         final_set.append(point)
+     
+    # make it a complete loop
+    final_set.append(final_set[0])
         
     return final_set
 
@@ -227,32 +235,62 @@ def make_rand_vector(dims):
     return [x/mag for x in vec]
 
 def fix_angles(points, max_angle=MAX_ANGLE):
+    
     for i in range(len(points)):
+        
+        # set the previous point
         if i > 0:
             prev_point = i - 1
+        
+        # set the previous point to the end of the list
         else:
             prev_point = len(points)-1
+        
+        # set the next point
         next_point = (i+1) % len(points)
+        
+        # get the difference between current point and previous point
         px = points[i][0] - points[prev_point][0]
         py = points[i][1] - points[prev_point][1]
+        
+        # get the distance
         pl = math.sqrt(px*px + py*py)
+        
+        # get norm of difference
         px /= pl
         py /= pl
+        
+        # compute the difference between the current point and the next point
         nx = -(points[i][0] - points[next_point][0])
         ny = -(points[i][1] - points[next_point][1])
+        
+        # get the distance
         nl = math.sqrt(nx*nx + ny*ny)
+        
+        # norm the distances
         nx /= nl
         ny /= nl  
+        
+        # calculate the angle of the corner
         a = math.atan2(px * ny - py * nx, px * nx + py * ny)
+        
+        # if angle is suitable continue
         if (abs(math.degrees(a)) <= max_angle):
             continue
+        
+        # get the difference beyond the max angle
         diff = math.radians(max_angle * math.copysign(1,a)) - a
+        
+        # recalculate a new position which fits constraint
         c = math.cos(diff)
         s = math.sin(diff)
         new_x = (nx * c - ny * s) * nl
         new_y = (nx * s + ny * c) * nl
+        
+        # update the points
         points[next_point][0] = int(points[i][0] + new_x)
         points[next_point][1] = int(points[i][1] + new_y)
+        
     return points
 
 
@@ -260,93 +298,34 @@ def push_points_apart(points, distance=DISTANCE_BETWEEN_POINTS):
     
     for i in range(len(points)):
         for j in range(i+1, len(points)):
-            p_distance =  math.sqrt((points[i][0]-points[j][0])**2 + (points[i][1]-points[j][1])**2)
+            
+            # get distance between 2 points 
+            p_distance =  math.sqrt((points[i][0] - points[j][0]) ** 2 + (points[i][1] - points[j][1]) ** 2)
+            
+            # if beyond the max distance
             if p_distance < distance:
-                dx = points[j][0] - points[i][0];  
-                dy = points[j][1] - points[i][1];  
-                dl = math.sqrt(dx*dx + dy*dy);  
-                dx /= dl;  
-                dy /= dl;  
-                dif = distance - dl;  
-                dx *= dif;  
-                dy *= dif;  
-                points[j][0] = int(points[j][0] + dx);  
-                points[j][1] = int(points[j][1] + dy);  
-                points[i][0] = int(points[i][0] - dx);  
-                points[i][1] = int(points[i][1] - dy);  
+                
+                # get coord differences
+                dx = points[j][0] - points[i][0]  
+                dy = points[j][1] - points[i][1]  
+                dl = math.sqrt(dx*dx + dy*dy)  
+                
+                # norm the distances
+                dx /= dl  
+                dy /= dl
+                
+                # get the difference and update the coords
+                dif = distance - dl
+                dx *= dif  
+                dy *= dif  
+                
+                # updated coords in array
+                points[j][0] = int(points[j][0] + dx)
+                points[j][1] = int(points[j][1] + dy)  
+                points[i][0] = int(points[i][0] - dx)  
+                points[i][1] = int(points[i][1] - dy) 
+                    
     return points
-
-def get_corners_with_kerb(points, min_kerb_angle=MIN_KERB_ANGLE, max_kerb_angle=MAX_KERB_ANGLE):
-    require_kerb = []
-    for i in range(len(points)):
-        if i > 0:
-            prev_point = i - 1
-        else:
-            prev_point = len(points)-1
-        next_point = (i+1) % len(points)
-        px = points[prev_point][0] - points[i][0]
-        py = points[prev_point][1] - points[i][1]
-        pl = math.sqrt(px*px + py*py)
-        px /= pl
-        py /= pl
-        nx = points[next_point][0] - points[i][0]
-        ny = points[next_point][1] - points[i][1]
-        nl = math.sqrt(nx*nx + ny*ny)
-        nx /= nl
-        ny /= nl 
-        a = math.atan(px * ny - py * nx)
-        if (min_kerb_angle <= abs(math.degrees(a)) <= max_kerb_angle):
-            require_kerb.append(points[i])
-    return require_kerb
-
-def smooth_track(track_points):
-    x = np.array([p[0] for p in track_points])
-    y = np.array([p[1] for p in track_points])
-
-    # append the starting x,y coordinates
-    x = np.r_[x, x[0]]
-    y = np.r_[y, y[0]]
-    
-    # TODO: replace this interpolate function with a homemade versio
-
-    # fit splines to x=f(u) and y=g(u), treating both as periodic. also note that s=0
-    # is needed in order to force the spline fit to pass through all the input points.
-    tck, u = interpolate.splprep([x, y], s=0, per=True)
-
-    # evaluate the spline fits for # points evenly spaced distance values
-    xi, yi = interpolate.splev(np.linspace(0, 1, SPLINE_POINTS), tck)
-    return [(int(xi[i]), int(yi[i])) for i in range(len(xi))]
-
-def get_full_corners(track_points, corners):
-    # get full range of points that conform the corner
-    offset = FULL_CORNER_NUM_POINTS
-    corners_in_track = get_corners_from_kp(track_points, corners)
-    # for each corner keypoint in smoothed track, 
-    # get the set of points that make the corner.
-    # This are the offset previous and offset next points
-    f_corners = []
-    for corner in corners_in_track:
-        # get kp index
-        i = track_points.index(corner)
-        # build temp list to get set of points
-        tmp_track_points = track_points + track_points + track_points
-        f_corner = tmp_track_points[i+len(track_points)-1-offset:i+len(track_points)-1+offset]
-        f_corners.append(f_corner)
-    return f_corners
-
-def get_corners_from_kp(complete_track, corner_kps):
-    # for each detected corner find closest point in final track (smoothed track)
-    return [find_closest_point(complete_track, corner) for corner in corner_kps]
-
-def find_closest_point(points, keypoint):
-    min_dist = None
-    closest_point = None
-    for p in points:
-        dist = math.hypot(p[0]-keypoint[0], p[1]-keypoint[1])
-        if min_dist is None or dist < min_dist:
-            min_dist = dist
-            closest_point = p
-    return closest_point
 
 def get_checkpoints(track_points, n_checkpoints=N_CHECKPOINTS):
     # get step between checkpoints
@@ -363,8 +342,10 @@ def get_checkpoints(track_points, n_checkpoints=N_CHECKPOINTS):
 ####
 
 
-def draw_track(surface, color, points, corners):
+def draw_track(surface, color, points):
+    
     radius = TRACK_WIDTH // 2
+    
     # draw track
     chunk_dimensions = (radius * 2, radius * 2)
     for point in points:
@@ -372,11 +353,14 @@ def draw_track(surface, color, points, corners):
         track_chunk = pygame.Surface(chunk_dimensions, pygame.SRCALPHA)
         pygame.draw.circle(track_chunk, color, (radius, radius), radius)
         surface.blit(track_chunk, blit_pos)
+        
     starting_grid = draw_starting_grid(radius*2)
+    
     # rotate and place starting grid
     offset = TRACK_POINT_ANGLE_OFFSET
     vec_p = [points[offset][1] - points[0][1], -(points[offset][0] - points[0][0])]
     n_vec_p = [vec_p[0] / math.hypot(vec_p[0], vec_p[1]), vec_p[1] / math.hypot(vec_p[0], vec_p[1])]
+    
     # compute angle
     angle = math.degrees(math.atan2(n_vec_p[1], n_vec_p[0]))
     rot_grid = pygame.transform.rotate(starting_grid, -angle)
@@ -429,7 +413,36 @@ def get_orientation(origin, p1, p2):
         - ((p1[0] - origin[0]) * (p2[1] - origin[1]))
     )
 
-    return difference
+    return difference    
+
+
+def smooth_track(track_points):    
+    
+    # get x and y in the appropriate format
+    x, y = zip(*track_points)
+    x, y = list(x), list(y)
+    
+    k, H = 3, len(x) - 1
+    
+    print('D_n: {}'.format(len(x)))
+    print('H: {}'.format(H))
+    print('k: {}'.format(k))    
+        
+    # combine the lists
+    combined_list = [x, y]
+        
+    p_centripetal = centripetal(len(x), combined_list)
+    knot = knot_vector(p_centripetal, k, len(x))
+    P_control = curve_approximation(D=combined_list, N=len(x), H=H, k=k, param=p_centripetal, knot=knot)
+
+    # P_control = combined_list
+    
+    p_piece = np.linspace(0, 1, SPLINE_POINTS)
+    p_centripetal_new = centripetal(H, P_control)
+    knot_new = knot_vector(p_centripetal_new, k, H)
+    P_piece = curve(P_control, H, k, p_piece, knot_new) 
+    
+    return [(P_piece[0][idx], P_piece[1][idx]) for idx in range(len(P_piece[0]))]      
     
 
 ####
@@ -444,22 +457,34 @@ def main(draw_checkpoints_in_track=True):
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     background_color = GRASS_GREEN
     screen.fill(background_color)
-    
-    random.seed(0)
+        
+    # set the seed
+    # random.seed(11)
 
     # generate a set of random points
     points = random_points()
     
     # calculate the convex hull of the random points
-    hull_points = convex_hull(points)
+    hull_points = convex_hull(points)    
     
+    # get the points of the track
     track_points = shape_track(hull_points)
-    corner_points = get_corners_with_kerb(track_points)
+    
+    # smooth the track points
     f_points = smooth_track(track_points)
-    # get complete corners from keypoints
-    corners = get_full_corners(f_points, corner_points)
+    
+    """    
+    x, y = zip(*f_points)    
+    plt.plot(y, x)
+    x_1, y_1 = zip(*track_points)  
+    plt.scatter(y_1, x_1)
+    plt.show()        
+    sladhjsasanda
+    """
+            
     # draw the actual track (road, kerbs, starting grid)
-    draw_track(screen, GREY, f_points, corners)
+    draw_track(screen, GREY, f_points)
+    
     # draw checkpoints
     checkpoints = get_checkpoints(f_points)
 
@@ -474,7 +499,148 @@ def main(draw_checkpoints_in_track=True):
                 pygame.quit()
                 sys.exit()
         pygame.display.update()
+        
+        
+def curve(P, N, k, param, knot):
+    '''
+    Calculate B-spline curve.
+    :param P: Control points
+    :param N: the number of control points
+    :param k: degree
+    :param param: parameters
+    :param knot: knot vector
+    :return: data point on the b-spline curve
+    '''
+    Nik = np.zeros((len(param), N))
 
+    for i in range(len(param)):
+        for j in range(N):
+            Nik[i][j] = BaseFunction(j, k+1, param[i], knot)
+    Nik[len(param)-1][N - 1] = 1
+    D = []
+    for i in range(len(P)):
+        D.append(np.dot(Nik, P[i]).tolist())
+    return D
+
+def BaseFunction(i, k, u, knot):
+    '''
+    Calculate base function using Cox-deBoor function.
+    :param i: index of base function
+    :param k: order (degree + 1)
+    :param u: parameter
+    :param knot: knot vector
+    :return: base function
+    '''
+    Nik_u = 0
+    if k == 1:
+        if u >= knot[i] and u < knot[i + 1]:
+            Nik_u = 1.0
+        else:
+            Nik_u = 0.0
+    else:
+        length1 = knot[i + k - 1] - knot[i]
+        length2 = knot[i + k] - knot[i + 1]
+        if not length1 and not length2:
+            Nik_u = 0
+        elif not length1:
+            Nik_u = (knot[i + k] - u) / length2 * BaseFunction(i + 1, k - 1, u, knot)
+        elif not length2:
+            Nik_u = (u - knot[i]) / length1 * BaseFunction(i, k - 1, u, knot)
+        else:
+            Nik_u = (u - knot[i]) / length1 * BaseFunction(i, k - 1, u, knot) + \
+                    (knot[i + k] - u) / length2 * BaseFunction(i + 1, k - 1, u, knot)
+    return Nik_u
+
+def centripetal(n, P):
+    '''
+    Calculate parameters using the centripetal method.
+    :param n: the number of data points
+    :param P: data points
+    :return: parameters
+    '''
+    a = 0.5
+    parameters = np.zeros((1, n))
+    for i in range(1, n):
+        dis = 0
+        
+        for j in range(len(P)):
+            dis = dis + (P[j][i]-P[j][i-1]) ** 2
+            
+        dis = np.sqrt(dis)
+        parameters[0][i] = parameters[0][i-1] + np.power(dis, a)
+        
+    for i in range(1, n):
+        parameters[0][i] = parameters[0][i] / parameters[0][n-1]
+        
+    return parameters[0]
+
+def knot_vector(param, k, N):
+    '''
+    Generate knot vector.
+    :param param: parameters
+    :param k: degree
+    :param N: the number of data points
+    :return: knot vector
+    '''
+    m = N + k
+    knot = np.zeros((1, m+1))
+    for i in range(k + 1):
+        knot[0][i] = 0
+        
+    for i in range(m - k, m + 1):
+        knot[0][i] = 1
+        
+    for i in range(k + 1, m - k):
+        for j in range(i - k, i):
+            knot[0][i] = knot[0][i] + param[j]
+        knot[0][i] = knot[0][i] / k
+        
+    return knot[0]
+
+def curve_approximation(D, N, H, k, param, knot):
+    '''
+    Given a set of N data points, D0, D1, ..., Dn, a degree k,
+    and a number H, where N > H > k >= 1, find a B-spline curve
+    of degree k defined by H control points that satisfies the
+    following conditions:
+        1. this curve contains the first and last data points;
+        2. this curve approximates the data polygon in the sense
+        of least square;
+    :param D: data points (N x 2)
+    :param H: the number of control points
+    :param k: degree
+    :param param: parameters
+    :param knot: knot vector
+    :return: control points (H x 2)
+    '''
+    P = []
+    if H >= N or H <= k:
+        print('Parameter H is out of range')
+        return P
+
+    for idx in range(len(D)):
+        P_ = np.zeros((1, H))
+        P_[0][0] = D[idx][0]
+        P_[0][H-1] = D[idx][N-1]
+        Qk = np.zeros((N - 2, 1))
+        Nik = np.zeros((N, H))
+        
+        for i in range(N):
+            for j in range(H):
+                Nik[i][j] = BaseFunction(j, k + 1, param[i], knot)
+        # print(Nik)
+
+        for j in range(1, N - 1):
+            Qk[j - 1] = D[idx][j] - Nik[j][0] * P_[0][0] - Nik[j][H - 1] * P_[0][H - 1]
+
+        N_part = Nik[1: N - 1, 1: H - 1]
+        Q = np.dot(N_part.transpose(), Qk)
+        M = np.dot(np.transpose(N_part), N_part)
+        P_[0][1:H - 1] = np.dot(np.linalg.inv(M), Q).transpose()
+        P.append(P_.tolist()[0])
+
+    return P
+        
 if __name__ == '__main__':
     main(draw_checkpoints_in_track=True)
 
